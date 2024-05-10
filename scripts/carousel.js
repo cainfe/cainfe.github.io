7b3576391288be4e2e57ba3feb4535c0d0cbdcb9
floydc183 <70218160+cainfe@users.noreply.github.com>
2024-05-10 16:42:36 -0500

    Added carousels
    Added a carousel for the experience section with support for other sections.

---
+++ scripts/carousel.js
@@ -0,0 +1,26 @@
+let slideshowPosMap = new Map();
+
+showSlides("experience-carousel", 0);
+
+function showSlides(slideshowID, slideIndex) {
+  let i;
+  let slideshow = document.getElementById(slideshowID);
+  let slides = slideshow.getElementsByClassName("slide");
+  let slideCounter = slideshow.getElementsByClassName("slide-counter")[0];
+
+  if (slideIndex > slides.length - 1) slideIndex = 0;
+  if (slideIndex < 0) slideIndex = slides.length - 1;
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+
+  slideshowPosMap.set(slideshowID, slideIndex);
+  slideCounter.innerHTML = `${slideIndex + 1} / ${slides.length}`;
+}
+
+function changeSlide(slideshowID, n) {
+  showSlides(slideshowID, slideshowPosMap.get(slideshowID) + n);
+}
