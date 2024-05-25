e4ddd885136cd5fcec63b93f5d4c64b50274fea0
cainfe <70218160+cainfe@users.noreply.github.com>
2024-05-24 19:38:07 -0500

    Created zoomable images
    Added modal so that users can click on an image to "zoom in".

---
+++ scripts/imageZoom.js
@@ -0,0 +1,27 @@
+let images = document.querySelectorAll('.zoomable-img');
+let modal = document.getElementById('zoom-modal');
+let modalImg = document.getElementById('zoom-modal-img');
+let modalCaption = document.getElementById('zoom-modal-caption');
+let closeButton = document.getElementsByClassName('zoom-modal-close')[0];
+
+images.forEach(img => {
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalCaption.innerHTML = this.alt;
+    }
+});
+
+closeButton.onclick = function() {
+    modal.style.display = "none";
+}
+
+modal.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+modalImg.onclick = function(event) {
+    event.stopPropagation();
+}
